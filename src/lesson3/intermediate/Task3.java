74260daa066b4532a635dc9a0428d546073b3293
anhelina89 <angelinapodushkina@gmail.com>
2017-02-01 22:01:08 +0200

    +2 tasks

--- src/lesson3/intermediate/Task3.java
+++ src/lesson3/intermediate/Task3.java
@@ -5,22 +5,16 @@ package lesson3.intermediate;
  */
 public class Task3 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        int[] arr = {1,2,3,4,5,6};
 
-        int temp1=arr.length-1;
-
-
-        for (int i=0; i<arr.length; i++){
-
-               int temp = arr[i];
-               arr[i] = arr[temp1];
-               arr[temp1] = temp;
-
-               temp1 = temp1 - 1;
-
-            System.out.println(arr[i]);
+        for (int i=0; i< arr.length/2;i++){
+            int j = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i]=j;
         }
-
+        for(int m=0; m < arr.length;m++){
+            System.out.print(arr[m]+" ");
+        }
     }
 
 }
